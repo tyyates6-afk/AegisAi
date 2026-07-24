dd0f5d068d0652ffa094aa6345cb71158b39fc80
tyyates6-afk <tyyates6@icloud.com>
2026-07-24 19:13:36 -0400

    2.1

--- JS/app.js
+++ JS/app.js
@@ -91,3 +91,25 @@ setInterval(
     updateDashboard,
     1000
 );
+
+if ("serviceWorker" in navigator) {
+
+window.addEventListener("load", () => {
+
+navigator.serviceWorker.register("service-worker.js")
+
+.then(() => {
+
+console.log("✓ Service Worker Registered");
+
+})
+
+.catch(err => {
+
+console.error(err);
+
+});
+
+});
+
+}
---
+++ JS/service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = "aegis-v2";
+
+const FILES = [
+
+"/",
+
+"/index.html",
+
+"/style.css",
+
+"/manifest.json",
+
+"/js/core.js",
+
+"/js/storage.js",
+
+"/js/app.js"
+
+];
+
+self.addEventListener("install", event => {
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+.then(cache => {
+
+return cache.addAll(FILES);
+
+})
+
+);
+
+});
+
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response => {
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
