89225a722eed9ceeec6393c580e7d43a50fab828
tyyates6-afk <tyyates6@icloud.com>
2026-07-24 21:20:28 -0400

    2.1.5

--- JS/app.js
+++ JS/app.js
@@ -25,61 +25,7 @@ function getGreeting(){
 
 
 
-function updateDashboard(){
 
-
-    const profile =
-    loadData("profile")[0] || {name:"User"};
-
-
-
-    const now =
-    new Date();
-
-
-
-    const date =
-    now.toLocaleDateString(
-        undefined,
-        {
-            weekday:"long",
-            year:"numeric",
-            month:"long",
-            day:"numeric"
-        }
-    );
-
-
-
-    const time =
-    now.toLocaleTimeString();
-
-
-
-    document.getElementById(
-        "status"
-    ).innerHTML = `
-
-    ${getGreeting()}, 
-    ${profile.name || "User"}.
-
-    <br><br>
-
-    ${date}
-
-    <br>
-
-    ${time}
-
-    <br><br>
-
-    SYSTEM STATUS:
-    ONLINE
-
-    `;
-
-
-}
 
 
 
