683249bff14b19cd8c3d5417b166f3cfd7720f5f
tyyates6-afk <tyyates6@icloud.com>
2026-08-08 18:31:44 -0500

    3.12

--- JS/app.js
+++ JS/app.js
@@ -28,7 +28,7 @@ if ("serviceWorker" in navigator) {
 
 window.addEventListener("load", () => {
 
-navigator.serviceWorker.register("JS/service-worker.js")
+navigator.serviceWorker.register("service-worker.js")
 
 .then(() => {
 
